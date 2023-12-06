0a250e91f027885f49f386cff63db29a8df2bac6
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-12-06 23:36:21 +0330

    feat: implement course modal widget

---
+++ lib/modal_screen/course_modal.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:practice_isar_db/entities/course.dart';
+
+import '../isar_service.dart';
+
+class CourseModal extends StatefulWidget {
+  final IsarService isarService;
+  const CourseModal({required this.isarService, super.key});
+
+  @override
+  State<CourseModal> createState() => _CourseModalState();
+}
+
+class _CourseModalState extends State<CourseModal> {
+  final TextEditingController _textController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _textController,
+              autofocus: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Course name is not allowed to be empty';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    widget.isarService
+                        .saveCourse(Course()..title = _textController.text);
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                            'New Course *${_textController.text}* saved in DB')));
+                  }
+                },
+                child: const Text('Add new Course'))
+          ],
+        ));
+  }
+}
